58efb576b329957f8d28a260fa19fcc835dc9619
Pervez Iqbal <pervezfunctor@gmail.com>
2023-03-09 22:54:41 +0530

    add test for compose

---
+++ src/compose.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from 'vitest'
+import { compose } from './compose'
+
+test('compose', () => {
+  const add1 = (x: number) => x + 1
+  const mul3 = (x: number) => x * 3
+  const add1Mul3 = compose(mul3, add1)
+  expect(add1Mul3(1)).toBe(6)
+})
