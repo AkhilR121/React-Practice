fc7dd97c11b3bc4a1c2ab1ef230f7886fdcd4858
Pervez Iqbal <pervezfunctor@gmail.com>
2023-03-09 20:19:23 +0530

    remove factorial

--- src/factorial.test.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from 'vitest'
-import { factorial } from './factorial'
-
-test('example', () => {
-  expect(factorial(0)).toBe(1)
-  expect(factorial(1)).toBe(1)
-  expect(factorial(5)).toBe(120)
-  expect(() => factorial(-1)).toThrow()
-})
