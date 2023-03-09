04a70f8a41a80185ae23f6b4deabeb66544b4839
Pervez Iqbal <pervezfunctor@gmail.com>
2023-03-09 20:16:53 +0530

    disabe no-explicit-any rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
     'prettier/prettier': 'error',
     eqeqeq: 'error',
   },
