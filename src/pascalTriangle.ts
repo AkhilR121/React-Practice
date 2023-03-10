584912425e1512527a2a27636f401560fc42d740
Pervez Iqbal <pervezfunctor@gmail.com>
2023-03-10 19:55:09 +0530

    minor changes

--- src/pascalTriangle.ts
+++ src/pascalTriangle.ts
@@ -1,13 +1,9 @@
-// pascalTriangle
-// pascalLine
-// ncr
-// factorial
-
 export function factorial(n: number): number {
   let fact = 1
   for (let i = 1; i <= n; i += 1) {
     fact *= i
   }
+
   return fact
 }
 
@@ -16,10 +12,9 @@ function ncr(n: number, r: number): number {
 }
 
 type PascalLine = number[]
-// 3c0 3c1 3c2 3c3
+
 function pascalLine(line: number): PascalLine {
   const result: number[] = []
-
   for (let i = 0; i <= line; i += 1) {
     result.push(ncr(line, i))
   }
@@ -28,7 +23,6 @@ function pascalLine(line: number): PascalLine {
 
 export function pascalTriangle(n: number): PascalLine[] {
   const result: PascalLine[] = []
-
   for (let i = 0; i <= n; i += 1) {
     result.push(pascalLine(i))
   }
