7fe67c08a0aaa74ac77590943639bf2e0def398c
Pervez Iqbal <pervezfunctor@gmail.com>
2023-03-18 00:23:44 +0530

    add range function

--- src/range.ts
+++ src/range.ts
@@ -1,6 +1,20 @@
-export const range = (start: number, stop: number) => {
+import invariant from 'tiny-invariant'
+import { Natural, Positive, verify } from './specs'
+
+export function range(start: number, stop?: Natural, step?: number): number[] {
+  if (stop === undefined) {
+    stop = start
+    start = 0
+  }
+  if (step === undefined) {
+    step = 1
+  }
+
+  verify(Positive, step)
+  invariant(start <= stop, `start(${start}) must be less than stop(${stop})`)
+
   const result = []
-  for (let i = start; i < stop; i++) {
+  for (let i = start; i < stop; i += step) {
     result.push(i)
   }
 
