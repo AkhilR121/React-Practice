5fde61fe02fe18c18e8d521547fea2a868c63acd
Pervez Iqbal <pervezfunctor@gmail.com>
2023-03-18 00:22:43 +0530

    add verify and is; few necessary specs

---
+++ src/specs.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod'
+
+export const Natural = z.number().int().nonnegative()
+export type Natural = z.infer<typeof Natural>
+
+export const Positive = z.number().int().nonnegative()
+export type Positive = z.infer<typeof Positive>
+
+export const Int = z.number().int()
+export type Int = z.infer<typeof Int>
+
+export function verify<T>(
+  schema: z.ZodSchema<T>,
+  value: unknown,
+  msg?: string,
+): T | Error {
+  const result = schema.safeParse(value)
+  return result.success ? result.data : msg ? new Error(msg) : result.error
+}
+
+export function is<T>(schema: z.ZodSchema<T>, value: unknown): value is T {
+  return schema.safeParse(value).success
+}
