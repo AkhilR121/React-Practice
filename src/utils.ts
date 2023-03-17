cf37ce7f42387013272927894c3eeb82f7293888
Pervez Iqbal <pervezfunctor@gmail.com>
2023-03-18 00:22:20 +0530

    add tap and bool utils functions

---
+++ src/utils.ts
@@ -0,0 +1,9 @@
+export const tap =
+  (msg?: string) =>
+  <T>(x: T): T => {
+    // eslint-disable-next-line no-console
+    console.log(`tap(${msg ?? ''}): ${x}`)
+    return x
+  }
+
+export const bool = (e: any) => !!e
