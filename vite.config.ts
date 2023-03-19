16905b79990efaa941f3e18e8471d08cea32cf52
Pervez Iqbal <pervezfunctor@gmail.com>
2023-03-19 23:27:11 +0530

    add support for code coverage using c8

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    coverage: {
+      provider: 'c8',
+      reporter: ['lcov'],
+    },
     includeSource: ['src/**/*.{tsx,ts}'],
   },
 })
