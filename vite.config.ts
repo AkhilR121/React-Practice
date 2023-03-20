000ff5279d077d979bce18bf0eabfe5f2ac8c6fd
Pervez Iqbal <pervezfunctor@gmail.com>
2023-03-20 23:15:43 +0530

    enable typescript, eslint checking in dev script

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 // vite.config.ts
 import { defineConfig } from 'vitest/config'
+import checker from 'vite-plugin-checker'
 
 export default defineConfig({
+  plugins: [
+    checker({
+      // e.g. use TypeScript check
+      typescript: true,
+      eslint: {
+        // for example, lint .ts and .tsx
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
+    }),
+  ],
   test: {
     coverage: {
       provider: 'c8',
