97177868f6354279e476db47a3bba307a76f19f8
Pervez Iqbal <pervezfunctor@gmail.com>
2023-03-22 15:37:22 +0530

    move checked to specs

--- src/lib/specs.ts
+++ src/lib/specs.ts
@@ -37,3 +37,18 @@ export function is<T>(schema: z.ZodSchema<T>, value: unknown): value is T {
 }
 
 export type Comparable = string | number | Date
+
+export function checked<
+  Args extends [] | [z.ZodTypeAny, ...z.ZodTypeAny[]],
+  F extends (...args: z.infer<Args[number]>[]) => unknown,
+>(specs: Args, f: F) {
+  return z.function(z.tuple(specs)).implement(f)
+}
+
+export function checkedReturn<
+  Args extends [] | [z.ZodTypeAny, ...z.ZodTypeAny[]],
+  F extends (...args: z.infer<Args[number]>[]) => unknown,
+  R extends z.ZodTypeAny,
+>(specs: Args, ret: R, f: F) {
+  return z.function(z.tuple(specs), ret).implement(f)
+}
