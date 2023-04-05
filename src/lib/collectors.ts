5582c2ddfe6ccb6099cc6370c904961c86632fe4
Pervez Iqbal <pervezfunctor@gmail.com>
2023-04-06 01:53:57 +0530

    add collector functions

---
+++ src/lib/collectors.ts
@@ -0,0 +1,42 @@
+import invariant from "tiny-invariant";
+import { reduce } from "./iter";
+import { plus } from "./operators";
+import { pipe } from "./pipe";
+
+export function each<T>(f: (x: T) => void) {
+  return (arr: Iterable<T>): void => {
+    for (const e of arr) {
+      f(e);
+    }
+  };
+}
+
+export const sum = (arr: Iterable<number>): number =>
+  pipe(arr, reduce(plus, 0));
+
+export const max = (arr: Iterable<number>): number => {
+  const [first, ...rest] = arr;
+  invariant(first, "max: empty array");
+
+  return pipe(rest, reduce(Math.max, first));
+};
+
+export const min = (arr: Iterable<number>): number => {
+  const [first, ...rest] = arr;
+  invariant(first, "min: empty array");
+
+  return pipe(rest, reduce(Math.min, first));
+};
+
+export const toArray = <T>(arr: Iterable<T>): T[] => [...arr];
+
+export const toSet = <T>(arr: Iterable<T>): Set<T> => new Set(arr);
+
+export const toMap = <K, V>(arr: Iterable<[K, V]>): Map<K, V> => new Map(arr);
+
+export const count = <T>(arr: Iterable<T>): number =>
+  pipe(
+    arr,
+
+    reduce(acc => acc + 1, 0)
+  );
