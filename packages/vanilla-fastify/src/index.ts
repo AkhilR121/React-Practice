8c617b1395a9a5ed89d8296ec6195debd81005d0
Pervez Iqbal <pervezfunctor@gmail.com>
2023-04-16 15:19:25 +0530

    basic fastify api project

---
+++ packages/vanilla-fastify/src/index.ts
@@ -0,0 +1,26 @@
+import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+import { Static, Type } from "@sinclair/typebox";
+import fastify from "fastify";
+import { ping } from "./routes";
+
+export const User = Type.Object({
+  name: Type.String(),
+  mail: Type.Optional(Type.String({ format: "email" })),
+});
+
+export type UserType = Static<typeof User>;
+
+const app = fastify({
+  logger: { transport: { target: "pino-pretty" } },
+}).withTypeProvider<TypeBoxTypeProvider>();
+
+app.register(ping);
+
+app.listen({ port: 8080 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log(`Server listening at ${address}`);
+});
---
+++ packages/vanilla-fastify/src/routes/ping.ts
@@ -0,0 +1,7 @@
+import { FastifyPluginAsync } from "fastify";
+
+export const ping: FastifyPluginAsync = async (fastify): Promise<void> => {
+  fastify.get("/ping", async function () {
+    return "pong\n";
+  });
+};
