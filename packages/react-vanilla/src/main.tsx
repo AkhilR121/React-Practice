f59dc76bc1423360bc0f65192c76f3436c62eb2e
Pervez Iqbal <pervezfunctor@gmail.com>
2023-04-18 23:35:51 +0530

    setup Suspense and ErrorBoundary

--- packages/react-vanilla/src/main.tsx
+++ packages/react-vanilla/src/main.tsx
@@ -1,9 +1,32 @@
+import { Box, Heading } from "@chakra-ui/react";
+import { Suspense } from "react";
 import ReactDOM from "react-dom/client";
+import { ErrorBoundary, type ErrorBoundaryProps } from "react-error-boundary";
+import { Toaster } from "react-hot-toast";
+import { SWRConfig } from "swr";
 import invariant from "tiny-invariant";
 import { App } from "./App";
-
 const rootEl = document.getElementById("root");
 invariant(rootEl, "root element not found");
 
+const ErrorFallback: ErrorBoundaryProps["FallbackComponent"] = ({ error }) => (
+  <Heading size="3xl" color="red.700">
+    {error.message}
+  </Heading>
+);
+
+const SuspenseFallback = <Heading size="3xl">Loading...</Heading>;
+
 const root = ReactDOM.createRoot(rootEl);
-root.render(<App />);
+root.render(
+  <Box>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <Suspense fallback={SuspenseFallback}>
+        <SWRConfig value={{ suspense: true }}>
+          <App />
+          <Toaster />
+        </SWRConfig>
+      </Suspense>
+    </ErrorBoundary>
+  </Box>
+);
