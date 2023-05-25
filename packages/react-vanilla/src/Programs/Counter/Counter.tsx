07c8a7d4a536259bc5cc6a45b726267c478dfd2a
Shibashis <shibashis.deb@technoidentity.com>
2023-05-25 14:48:30 +0530

    fix: counter using reducer

--- packages/react-vanilla/src/Programs/Counter/Counter.tsx
+++ packages/react-vanilla/src/Programs/Counter/Counter.tsx
@@ -1,22 +1,41 @@
-import { Button, VStack } from "@chakra-ui/react";
-import { useState } from "react";
+import { Button, HStack, VStack } from "@chakra-ui/react";
+import React from "react";
+import { produce } from "immer";
+
+type State = {
+  count: number;
+};
+
+type Action = {
+  type: "inc" | "dec";
+};
+
+function counterReducer(state: State, action: Action) {
+  return action.type === "inc"
+    ? produce(state, draft => ({ count: draft.count + 1 }))
+    : produce(state, draft => ({ count: draft.count - 1 }));
+}
 
 export function Counter() {
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = React.useReducer(counterReducer, { count: 0 });
 
   const incCount = () => {
-    setCount(count => count + 1);
+    dispatch({ type: "inc" });
   };
 
   const decCount = () => {
-    setCount(count => count - 1);
+    dispatch({ type: "dec" });
   };
 
   return (
     <VStack>
-      <Button onClick={incCount}>+</Button>
-      <div>{count}</div>
-      <Button onClick={decCount}>-</Button>
+      <HStack spacing="25px">
+        <div>{state.count}</div>
+        <VStack spacing="35px">
+          <Button onClick={incCount}>+</Button>
+          <Button onClick={decCount}>-</Button>
+        </VStack>
+      </HStack>
     </VStack>
   );
 }
