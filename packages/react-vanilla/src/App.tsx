1adab5c11f32b875b53be2665f868e79d43b5af8
Akhil V <akhilvinnakota1998@gmail.com>
2023-10-21 23:19:51 +0530

    delete files

--- packages/react-vanilla/src/App.tsx
+++ packages/react-vanilla/src/App.tsx
@@ -11,8 +11,7 @@ import { TodoApp } from "./TodoApp/todo-app";
 import { TrelloApp } from "./Trello-App/TrelloApp";
 import { Counter } from "./counter/Counter";
 import { DataGridApp } from "./data-grid/dataGridApp";
-import { Login } from "./shopping-cart/Login";
-import { SignUp } from "./shopping-cart/SignUp";
+
 import { ShoppingCartApp } from "./shopping-cart/shoppingCart";
 
 const router = createBrowserRouter(
@@ -23,11 +22,7 @@ const router = createBrowserRouter(
       <Route path="/data-grid" element={<DataGridApp />}></Route>
       <Route path="timer" element={<TimerApp />}></Route>
       <Route path="query" element={<RQuery />}></Route>
-
       <Route path="/shopping-cart" element={<ShoppingCartApp />}></Route>
-      <Route path="/login" element={<Login />}>
-        <Route path="signup" element={<SignUp />}></Route>
-      </Route>
       <Route path="/multiStepForm" element={<MultiStepForm />} />
       <Route path="/trello" element={<TrelloApp />} />
     </Route>
--- packages/react-vanilla/vite.config.js
+++ packages/react-vanilla/vite.config.js
@@ -1,17 +1,16 @@
-"use strict";
-exports.__esModule = true;
-var plugin_react_swc_1 = require("@vitejs/plugin-react-swc");
-var vite_1 = require("vite");
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
 // https://vitejs.dev/config/
-exports["default"] = (0, vite_1.defineConfig)({
-    plugins: [(0, plugin_react_swc_1["default"])()],
-    server: {
-        proxy: {
-            "/api": {
-                target: "http://localhost:3001",
-                changeOrigin: true,
-                rewrite: function (path) { return path.replace(/^\/api/, ""); }
-            }
-        }
-    }
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
