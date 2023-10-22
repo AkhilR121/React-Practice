84af298d026e02ddb0c310ecda1e2efc1a5d2f3a
Akhil V <akhilvinnakota1998@gmail.com>
2023-10-22 19:34:09 +0530

    changes in routing with latest version

--- packages/react-vanilla/src/App.tsx
+++ packages/react-vanilla/src/App.tsx
@@ -1,33 +1,69 @@
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/">
+//       <Route path="/counter" element={<Counter />}></Route>
+//       <Route path="/TodoApp" element={<TodoApp />}></Route>
+//       <Route path="/data-grid" element={<DataGridApp />}></Route>
+//       <Route path="timer" element={<TimerApp />}></Route>
+//       <Route path="query" element={<RQuery />}></Route>
+//       <Route path="/shopping-cart" element={<ShoppingCartApp />}></Route>
+//       <Route path="/multiStepForm" element={<MultiStepForm />} />
+//       <Route path="/trello" element={<TrelloApp />} />
+//     </Route>
+//   )
+// );
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import { ErrorPage } from "./ErrorComp/ErrorPage";
 import { MultiStepForm } from "./MultiStepForm/MultiStepForm";
-import { RQuery } from "./React-query/react-query";
+import { Root } from "./RootComp/Root";
 import { TimerApp } from "./Timer/TimerApp";
 import { TodoApp } from "./TodoApp/todo-app";
 import { TrelloApp } from "./Trello-App/TrelloApp";
 import { Counter } from "./counter/Counter";
 import { DataGridApp } from "./data-grid/dataGridApp";
-
 import { ShoppingCartApp } from "./shopping-cart/shoppingCart";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/">
-      <Route path="/counter" element={<Counter />}></Route>
-      <Route path="/TodoApp" element={<TodoApp />}></Route>
-      <Route path="/data-grid" element={<DataGridApp />}></Route>
-      <Route path="timer" element={<TimerApp />}></Route>
-      <Route path="query" element={<RQuery />}></Route>
-      <Route path="/shopping-cart" element={<ShoppingCartApp />}></Route>
-      <Route path="/multiStepForm" element={<MultiStepForm />} />
-      <Route path="/trello" element={<TrelloApp />} />
-    </Route>
-  )
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "/counter",
+    element: <Counter />,
+  },
+  {
+    path: "/trello",
+    element: <TrelloApp />,
+  },
+  {
+    path: "/DataGridApp",
+    element: <DataGridApp />,
+  },
+  {
+    path: "/TodoApp",
+    element: <TodoApp />,
+  },
+  {
+    path: "/TimerApp",
+    element: <TimerApp />,
+  },
+  {
+    path: "/MultiStepForm",
+    element: <MultiStepForm />,
+  },
+  {
+    path: "/shoppingCart",
+    element: <ShoppingCartApp />,
+  },
+
+  // Error Page
+  {
+    path: "/*",
+    element: <ErrorPage />,
+  },
+]);
 export const App = () => {
   return <RouterProvider router={router} />;
 };
