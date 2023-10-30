147928d9c77f5179cfb4429e6024d3dab291c2e2
Akhil Vinnakota <AkhilVinnakota@users.noreply.gitlab.com>
2023-10-30 17:25:37 +0530

    added api call function

--- packages/react-vanilla/src/shopping-cart/fakerData.ts
+++ packages/react-vanilla/src/shopping-cart/fakerData.ts
@@ -1,4 +1,5 @@
 import { faker } from "@faker-js/faker";
+import { map, pipe, range, toArray } from "@ti/core";
 // import { map, pipe, range, toArray } from "@ti/core";
 
 faker.seed(99);
@@ -29,16 +30,22 @@ export type ItemsList = {
 //   id: 1,
 //   title,
 // };
-// export const fakeProducts: ItemsList[] = pipe(
-//   range(20),
-//   map(() => ({
-//     id: faker.number.int(),
-//     title: faker.commerce.productName(),
-//     image: faker.image.url(),
-//     price: Number(faker.commerce.price()),
-//     description: faker.commerce.productDescription(),
-//     rating: faker.number.float({ min: 1, max: 5, precision: 0.1 }),
-//     category: faker.commerce.productDescription(),
-//   })),
-//   toArray
-// );
+export const fakeProducts: ItemsList[] = pipe(
+  range(20),
+  map(() => ({
+    id: faker.number.int(),
+    title: faker.commerce.productName(),
+    image: faker.image.url(),
+    price: Number(faker.commerce.price()),
+    description: faker.commerce.productDescription(),
+    rating: faker.number.float({ min: 1, max: 5, precision: 0.1 }),
+    category: faker.commerce.productDescription(),
+  })),
+  toArray
+);
+
+export async function apiCall() {
+  return await fetch("https://fakestoreapi.com/products")
+    .then(res => res.json())
+    .then(date => date);
+}
