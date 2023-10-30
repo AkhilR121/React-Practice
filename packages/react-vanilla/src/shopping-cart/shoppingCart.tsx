9535d8c0ecbc8712283da896916fe2ba3f2bc81e
Akhil Vinnakota <AkhilVinnakota@users.noreply.gitlab.com>
2023-10-30 17:28:09 +0530

    added initialData to useQuery, changes in reducer

--- packages/react-vanilla/src/shopping-cart/shoppingCart.tsx
+++ packages/react-vanilla/src/shopping-cart/shoppingCart.tsx
@@ -1,33 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import { Draft } from "immer";
-import { useState } from "react";
 import { useImmerReducer } from "use-immer";
-import { CartItems } from "./cart-items";
+import { apiCall, fakeProducts } from "./fakerData";
 import { Navbar } from "./navbar";
 import { ShoppingItems } from "./shoppingItems";
 
-async function apiCall() {
-  return await fetch("https://fakestoreapi.com/products").then(res =>
-    res.json()
-  );
-}
-
 export function ShoppingCartApp() {
-  const { data, isLoading } = useQuery({
+  const data = useQuery({
     queryKey: ["shop-list"],
     queryFn: () => apiCall(),
+    initialData: () => fakeProducts,
   });
-  console.log(data);
-
-  const [products] = useState(data);
   const [cartState, dispatch] = useImmerReducer<State, Action>(reducer, []);
+  console.log("App Comp", cartState);
 
   return (
     <div>
       <Navbar />
-      <div className="flex">
-        <ShoppingItems items={products} dispatch={dispatch} />
-        <CartItems products={products} state={cartState} dispatch={dispatch} />
+      <div className="">
+        <ShoppingItems items={data.data} dispatch={dispatch} />
+        {/* <CartItems products={data.data} state={cartState} dispatch={dispatch} /> */}
       </div>
     </div>
   );
@@ -40,44 +32,16 @@ type CartItem = {
 
 export type State = CartItem[];
 
-export type Action =
-  | {
-      type: "add_item";
-      id: number;
-    }
-  | {
-      type: "inc_item";
-      id: number;
-    }
-  | {
-      type: "dec_item";
-      id: number;
-    };
+export type Action = {
+  type: "add_item";
+  id: number;
+};
 
 function reducer(draft: Draft<State>, action: Action) {
   switch (action.type) {
     case "add_item": {
       if (!draft.find(ele => ele.id === action.id)) {
         draft.push({ id: action.id, quantity: 1 });
-      }
-      break;
-    }
-    case "inc_item": {
-      const item = draft.find(d => d.id === action.id);
-      if (item) {
-        item.quantity++;
-      }
-      break;
-    }
-    case "dec_item": {
-      const item = draft.find(d => d.id === action.id);
-      if (item) {
-        if (item.quantity === 1) {
-          const index = draft.findIndex(ci => ci.id === action.id);
-          draft.splice(index, index + 1);
-          break;
-        }
-        item.quantity--;
       }
       break;
     }
